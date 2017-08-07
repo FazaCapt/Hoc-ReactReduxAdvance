35b84162f2595c27c79cb8dd91373a1ece632293
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-07 21:35:01 +0700

    HOC Scaffold Code

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 export default function(ComposedComponent) {
     class Authentication extends Component {
         render() {
+            // console.log(this.props.resources) // => resourcesList
             return <ComposedComponent {...this.props} /> 
         }
     }
     return Authentication;
 }
+
+
+// // In some location... Not in this file...
+// // we want to use this HOC
+
+// import Authentication //this is my HOC
+// import Resources //this is the component i want to wrap
+
+// const ComposedComponent = Authentication(Resources);
+
+// // in some render method....
+// <ComposedComponent resources={resourcesList} />
